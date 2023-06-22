c56ef401fc3d4b0beb2ea01f358d89ee0a702901
Todd Bowden <toddhokie@gmail.com>
2023-06-22 08:49:12 -0400

    streamline CGImage+fillColor

--- Sources/TBImage/CGImage+fillColor.swift
+++ Sources/TBImage/CGImage+fillColor.swift
@@ -11,25 +11,10 @@ import CoreGraphics
 public extension CGImage {
     
     static func fill(color: CGColor, width: Int, height: Int) throws -> CGImage {
-        let bitmapInfoRawValue = CGBitmapInfo.byteOrder32Big.rawValue | CGImageAlphaInfo.premultipliedLast.rawValue
-        let bitmapInfo = CGBitmapInfo(rawValue: bitmapInfoRawValue)
-        
-        let context = CGContext(data: nil,
-                                width: width,
-                                height: height,
-                                bitsPerComponent: 8,
-                                bytesPerRow: width*4,
-                                space: CGColorSpaceCreateDeviceRGB(),
-                                bitmapInfo: bitmapInfo.rawValue)
-        guard let context else {
-            throw TBImageError.errorCreatingCGContext
-        }
+        let context = try CGContext.rgbaContext(width: width, height: height)
         context.setFillColor(color)
         context.fill(CGRect(x: 0, y: 0, width: width, height: height))
-        guard let cgImage = context.makeImage() else {
-            throw TBImageError.errorCreatingCGImage
-        }
-        return cgImage
+        return try context.image()
     }
     
 }
