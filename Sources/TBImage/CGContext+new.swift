02c107a36614a2c886ad7494f14d73fded1914fb
Todd Bowden <toddhokie@gmail.com>
2023-06-22 08:44:54 -0400

    add CGContext+new

---
+++ Sources/TBImage/CGContext+new.swift
@@ -0,0 +1,47 @@
+//
+//  CGContext+new.swift
+//
+//
+//  Created by Todd Bowden on 6/22/23.
+//
+
+import Foundation
+import CoreGraphics
+
+extension CGContext {
+    
+    static func grayContext(width: Int, height: Int) throws -> CGContext {
+        let context = CGContext(
+            data: nil,
+            width: width,
+            height: height,
+            bitsPerComponent: 8,
+            bytesPerRow: width,
+            space: CGColorSpaceCreateDeviceGray(),
+            bitmapInfo: 0
+        )
+        guard let context else {
+            throw TBImageError.errorCreatingCGContext
+        }
+        return context
+    }
+    
+    static func rgbaContext(width: Int, height: Int) throws -> CGContext {
+        let bitmapInfoRawValue = CGBitmapInfo.byteOrder32Big.rawValue | CGImageAlphaInfo.premultipliedLast.rawValue
+        let bitmapInfo = CGBitmapInfo(rawValue: bitmapInfoRawValue)
+        
+        let context = CGContext(
+                data: nil,
+                width: width,
+                height: height,
+                bitsPerComponent: 8,
+                bytesPerRow: width*4,
+                space: CGColorSpaceCreateDeviceRGB(),
+                bitmapInfo: bitmapInfo.rawValue)
+        guard let context else {
+            throw TBImageError.errorCreatingCGContext
+        }
+        return context
+    }
+    
+}
