ee8868bd196a52c4d0469177be3a147939fdfd80
Todd Bowden <toddhokie@gmail.com>
2023-11-21 17:08:03 -0500

    add CGImage+border

--- Sources/TBImage/CGImage/CGImage+border.swift
+++ Sources/TBImage/CGImage/CGImage+border.swift
@@ -1,8 +1,24 @@
 //
-//  File.swift
-//  
+//  CGImage+border.swift
+//
 //
 //  Created by Todd Bowden on 11/21/23.
 //
 
 import Foundation
+import CoreGraphics
+
+public extension CGImage {
+    
+    func addBorder(_ border: Int, color: CGColor? = nil) throws -> CGImage {
+        let color = color ?? CGColor(red: 0, green: 0, blue: 0, alpha: 1)
+        let w = self.width + border + border
+        let h = self.height + border + border
+        let context = try CGContext.rgbaContext(width: w, height: h)
+        context.setFillColor(color)
+        context.fill(CGRect(x: 0, y: 0, width: w, height: h))
+        context.draw(self, in: CGRect(x: border, y: border, width: width, height: height))
+        return try context.image()
+    }
+    
+}
