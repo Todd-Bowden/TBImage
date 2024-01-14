bf7812832e6c4c55ae91a8bdc125087b6db71576
Todd Bowden <toddhokie@gmail.com>
2024-01-14 08:28:09 -0500

    scale size

--- Sources/TBImage/CGImage/CGImage+scale.swift
+++ Sources/TBImage/CGImage/CGImage+scale.swift
@@ -10,10 +10,18 @@ import CoreGraphics
 
 public extension CGImage {
     
+    func scale(size: CGSize) throws -> CGImage {
+        try scale(width: Int(size.width), height: Int(size.height))
+    }
+    
     func scale(width: Int, height: Int) throws -> CGImage {
         let context = try self.context(width: width, height: height)
         context.draw(self, in: CGRect(x: 0, y: 0, width: width, height: height))
         return try context.image()
+    }
+    
+    func scaleRGBA(size: CGSize) throws -> CGImage {
+        try scaleRGBA(width: Int(size.width), height: Int(size.height))
     }
     
     func scaleRGBA(width: Int, height: Int) throws -> CGImage {
