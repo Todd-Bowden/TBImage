17fb60a2ffcda6df7e18ddb36b9a88492f5c20d4
Todd Bowden <toddhokie@gmail.com>
2024-03-24 10:49:19 -0400

    add CGImage+filter

---
+++ Sources/TBImage/CGImage/CGImage+filter.swift
@@ -0,0 +1,28 @@
+//
+//  CGImage+filter.swift
+//
+//
+//  Created by Todd Bowden on 3/24/24.
+//
+
+import Foundation
+import CoreGraphics
+import CoreImage
+
+public extension CGImage {
+    
+    func filter(_ name: String) -> CGImage? {
+        let ciImage = CoreImage.CIImage(cgImage: self)
+        guard let filter = CIFilter(name: name) else { return nil }
+        filter.setDefaults()
+        filter.setValue(ciImage, forKey: kCIInputImageKey)
+        let context = CIContext(options: nil)
+        guard let outputImage = filter.outputImage else { return nil }
+        return context.createCGImage(outputImage, from: outputImage.extent)
+    }
+    
+    var invert: CGImage? {
+        self.filter("CIColorInvert")
+    }
+    
+}
