c3922345105a3c826c5c6187dcf1f9f81d252b8b
Todd Bowden <toddhokie@gmail.com>
2024-03-24 11:58:50 -0400

    update CGImage+filter

--- Sources/TBImage/CGImage/CGImage+filter.swift
+++ Sources/TBImage/CGImage/CGImage+filter.swift
@@ -21,8 +21,23 @@ public extension CGImage {
         return context.createCGImage(outputImage, from: outputImage.extent)
     }
     
+    func filter(_ name: String, parameters: [String : Any]) -> CGImage? {
+        let ciImage = CoreImage.CIImage(cgImage: self)
+        guard let filter = CIFilter(name: name, parameters: parameters) else { return nil }
+        filter.setValue(ciImage, forKey: kCIInputImageKey)
+        let context = CIContext(options: nil)
+        guard let outputImage = filter.outputImage else { return nil }
+        return context.createCGImage(outputImage, from: outputImage.extent)
+    }
+    
     var invert: CGImage? {
         self.filter("CIColorInvert")
     }
     
+    func saturation(_ s: CGFloat) -> CGImage? {
+        filter("CIColorControls", parameters: [kCIInputSaturationKey:s])
+    }
+    
 }
+
+
